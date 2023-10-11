433af80c71bf03e88982a2bb8352885962558e62
Lukas Wimmer <79705377+luki58@users.noreply.github.com>
2023-10-11 11:10:50 +0200

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -107,7 +107,7 @@ def smooth(data,sigma):
 
 ### --- Sience Grayscale Plot --- ###
 
-def plot_1set(data):
+def plot_fit_group(data):
     arr_referenc =  np.arange(len(data))
     fig, ax = plt.subplots(dpi=600)
     fig.set_size_inches(6, 6)
@@ -153,6 +153,46 @@ def plot_1set(data):
     plt.show()    
 
     return poly1d_fn
+
+def plot_a(data):
+    arr_referenc =  np.arange(len(data))
+    fig, ax = plt.subplots(dpi=600)
+    fig.set_size_inches(6, 6)
+    #x_peak = find_peaks(data,height=2.4)
+    #print(x_peak[0][0])
+    #fig.savefig('test2png.png', dpi=100)
+    #color pattern: '#00429d' blue, '#b03a2e' red, '#1e8449' green
+    ax.plot(arr_referenc,data, color='#00429d', linewidth=0.5)
+    
+    #adds a title and axes labels
+    #ax.set_title('')
+    plt.xlabel('Time [frames]')
+    plt.ylabel('Wavespeed [mm/s]')
+ 
+    #removing top and right borders
+    #ax.spines['top'].set_visible(False)
+    #ax.spines['right'].set_visible(False) 
+    
+    #Edit tick 
+    #ax.xaxis.set_minor_locator(MultipleLocator(125))
+    #ax.yaxis.set_minor_locator(MultipleLocator(.1))
+
+    #add vertical lines
+    #ax.axvline(left, linestyle='dashed', color='b');
+    #ax.axvline(right, linestyle='dashed', color='b');
+
+    #adds major gridlines
+    ax.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+    
+    #ax limit
+    ax.set_xlim(xmin=0)
+    #ax.set_ylim(ymax=20)
+    
+    #legend
+    #ax.legend(bbox_to_anchor=(1, 1), loc=1, frameon=False, fontsize=16)
+    
+    plt.show()    
+
 def bigplot_wavelen(data, data2, data3, data4):
     arr_referenc =  np.arange(len(data))
     arr_referenc2 =  np.arange(len(data2))
@@ -293,8 +333,10 @@ def grayscaleplot_dataset(dataset):
 
 #%%
 
+### - Estimate Group Velocity of Cloud-Head - ### 
+
 rng = 10 
-cutwidth = 150
+cutwidth = 100
 cut = 0
 
 for i in frames[:rng]:
@@ -303,9 +345,9 @@ cut = int(cut/rng)
 
 pos0 = []
 
-for x in group_frames:
-    prog = grey_sum(x[(cut-cutwidth):(cut+cutwidth),:] > 4)
-    peaks, _ = find_peaks(prog, distance=100, height=2)
+for x in group_frames[:30]:
+    prog = grey_sum(x[(cut-cutwidth):(cut+cutwidth),:] > 5)
+    peaks, _ = find_peaks(prog, distance=100, height=5)
     
     fig = plt.figure(figsize = (10,10), dpi=100) # create a 5 x 5 figure
     ax = fig.add_subplot(111)
@@ -319,8 +361,8 @@ for x in group_frames:
     else:
         print("No Peak found")
     
-result = plot_1set(pos0)
-print(result)
+result = plot_fit_group(pos0)
+print(str(result) + " * d/dx = v_ph")
 
 #%%
 
@@ -620,24 +662,24 @@ def gsc_wave_analysis(data, stepsize, peak_distance, peak_height):
 #The STEPSIZE is the parameter that specifies the time interval, in frames, over which the phase speed is measured. Recommended > 3.
 #!Stop-Start>=STEPSIZE!
 
-start = 21
-stop = 27
+start = 20
+stop = 200
 print('start: ' + str(start) + ' stop: '+str(stop))
 
 stepsize = 4
 peak_distance_max = 250
 peak_height_min = 0.1
 
-sl, wl = gsc_wave_analysis(frames[start:stop], stepsize, peak_distance_max, peak_height_min)
+sl, wl = gsc_wave_analysis(frames[start:], stepsize, peak_distance_max, peak_height_min)
 
 #%%
-plot_1set(wl)
+plot_a(slf15_gauss)
 #%%
 speed_list15 = sl
 wavelen_list15 = wl
 #%%
 speed_list15 = np.append(speed_list15,sl)
-wavelen_list15 = np.append(wavelen_list15,wl[:20])
+wavelen_list15 = np.append(wavelen_list15,wl)
 #%%
 sl, wl = np.append(speed_list30,propagationspeed(frames[15:40], background, 410))
 speed_list30 = np.append(speed_list30,sl)
@@ -673,7 +715,7 @@ wl20 = gaussian_filter1d(np.append(wavelen_list20, np.flip(wavelen_list20[-20:-1
 wl25 = gaussian_filter1d(wavelen_list25, sigma=1)
 wl30 = gaussian_filter1d(np.append(wavelen_list30, np.flip(wavelen_list30[-20:-1])), sigma=1)
 #%%
-bigplot_wavelen(wl15[20:98], wl20[15:], wl25[:98], wl30[:75])
+#bigplot_wavelen(wl15[20:98], wl20[15:], wl25[:98], wl30[:75])
 #bigplot_speed(speed_list, speed_list20, speed_list25[:300], speed_list30[:300])
 #bigplot_speed(slf15_gauss, slf20_extended, slf25_gauss, slf30_extended, error_15, error_20_extended, error_25, error_30_extended)
 #%%
@@ -709,6 +751,141 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, c)
 
 plt.show()
+
+#%%
+
+#Calculations theoretical dust accoustic wave speed based on given particle density
+
+k_b = 1.38065 * 10**(-23)   #m^2kg/s^2K
+eps_0 = 8.854 * 10**(-12)   #As/Vm
+e = 1.6022 * 10**(-19)      #C
+
+T_i = T_d = [0.06, 0.05, 0.035, 0.034] #eV
+T_iroom = [0.036, 0.034, 0.034, 0.032] #eV
+T_e = [9.8, 9.0, 8.7, 8.5]  #, 8.5, 8.3, 7.8, 7.5] #15pa 20pa 30pa 40pa 50pa 60pa 70pa 80pa at current 0.5mA in eV
+
+a = (1.3/2) *10**(-6) #micrometer particle radius
+a_mum = a * 10**(6)
+roh = 1574 #kg/m^3
+V = 4/3 * np.pi * a**3
+m_d = roh * V
+
+m_i = 20.1797 * 1.660539066 *10**(-27) #*u = kg
+
+z = 0.34 #=0.3 +-0.1 for neon
+n_d = [3.4, 3.8, 4.8, 4.3]
+n_d = np.multiply(n_d,10**11) #in m^-3
+V_td = (3*k_b*300/m_d)**(1/2) #(3*k_b*300/m_d)**(1/2) #particle thermal temperature
+
+
+R = 15 #mm
+r = [4.7, 5.8 ,3.5 ,4.2 ]
+n_e0 = [(0.8*scsp.jv(0,(2.4*r[0]/R))), (0.87*scsp.jv(0,(2.4*r[1]/R))), (0.92*scsp.jv(0,(2.4*r[2]/R))), (1.1*scsp.jv(0,(2.4*r[3]/R)))] #Antonivas n_e = n_e0*J(1,4r/R) bindable
+n_e0 = np.multiply(n_e0,10**14) #in m^-3
+
+# Particle charge
+Z_d = []
+for i in range(len(T_e)):
+    Z_d = np.append(Z_d,((4*np.pi*eps_0*k_b*T_e[i]*11600*a*z)/(e**2)))
+#
+#n_i0 = np.multiply(Z_d, n_d) # n_i = Z_d*n_d
+n_i0 = np.add(n_e0, np.multiply(Z_d, n_d)) # = n_e0 + Z_d*n_d melzer2019
+#N_i_r = np.multiply(n_i0,10**8)
+#
+#Debye electrons and ions
+debye_De = np.sqrt(np.divide(np.multiply((eps_0*k_b),np.multiply(T_e[i],11600)),np.multiply(n_e0,e**2)))
+debye_Di = np.sqrt(np.divide(np.multiply(eps_0*k_b*11600,T_i),np.multiply(n_i0,e**2)))
+debye_Dd = np.sqrt(np.divide(np.multiply(eps_0*k_b,V_td),np.multiply(n_d,e**2)))
+#
+#DA velocity
+aplha =(np.multiply(k_b*11600,T_i)/m_d)**(1/2)
+epsilon = np.divide((n_d),(n_i0))**(1/2)
+#
+C_daw = aplha * epsilon * Z_d * 10**(3) #mm/s
+C_dawlinear = (np.multiply(k_b*11600,T_iroom)/m_d)**(1/2) * epsilon * Z_d * 10**(3) #mm/s
+#
+w_pd = np.sqrt(np.divide((np.multiply(np.multiply(Z_d,Z_d),(n_d*e**2))),(m_d*eps_0)))
+debye_D = np.divide(np.multiply(debye_De,debye_Di),np.sqrt(debye_De**2 + debye_Di**2))
+#
+C_D = w_pd * debye_D
+#
+#w with boundary effects
+w_boundary = (w_pd*debye_D*2.4)/(0.015*(1+(debye_D**2/0.015**2))**(1/2))
+#
+#screening parameter?
+aaa = np.divide(a,debye_D)
+#screening strength
+b_ws = [0.0095, 0.01, 0.012, 0.012]
+kappa = np.divide(b_ws,debye_Di)
+#
+#Havnes Parameter
+#P = np.multiply(np.multiply(695*a_mum,T_e),np.divide(n_d,n_i0))
+P = np.multiply(Z_d,np.divide(n_d,n_i0))
+
+#non linear phase velocity
+wavelen = [0.01, 0.0035, 0.0033, 0.0022]
+k = np.divide(2*np.pi,wavelen)
+v_ph_0 = (np.divide((np.multiply(P,Z_d)),(1+(np.multiply(k,debye_Di))**2))+3)**(1/2)
+v_ph_1 = (np.divide(debye_D,debye_Dd)*(1/(1+(np.multiply(k,debye_D))**2))+3)**(1/2)
+
+#DIAW ????
+C_DIAW = np.sqrt(np.divide((np.multiply(n_i0,T_e)*k_b*11600),(m_i*n_e0)))
+
+#%%
+
+x = [15,20,25,30]
+x_fit = np.linspace(13, 27.5, 26)
+x_fit_lin = np.linspace(13, 32, 26)
+
+error = [7.5, 2.1, 3.4, 1.1]
+
+# --- DATA MODELING --- #
+def model(p, b, n):
+ return b + (n*p)**(-1/2) 
+
+popt, pcov = curve_fit(model, x, C_daw, sigma=[0.6,.2,.1,.2])
+b, n = popt
+
+c_model_adj = model(x_fit, b, n)
+
+
+popt_lin, pcov_lin = curve_fit(model, x , C_dawlinear, sigma=[0.2,.15,.1,.1])
+b_lin, n_lin = popt_lin
+
+c_model_lin = model(x_fit_lin, b_lin, n_lin)
+
+print("Adj:"+str(np.sqrt(np.diag(pcov)))+" Lin:"+str(np.sqrt(np.diag(pcov_lin))))
+
+print("AdjOpt (b,n):"+str(b)+" "+str(n)+" LinOpt (b,n):"+str(b_lin)+" "+str(n_lin))
+
+plt.imshow(np.log(np.abs(pcov)))
+plt.colorbar()
+plt.show
+
+
+#PLOT
+fig, ax = plt.subplots(dpi=600)
+fig.set_size_inches(4, 3)
+
+ax.errorbar(x, [82.1,70.8,59.3,57.2], yerr=error, fmt='^',color='#00429d', markersize=3, linewidth=1, capsize=1)
+ax.plot(x_fit, c_model_adj, linestyle='dashed', color='#00cc00', linewidth=.7)
+ax.plot(x_fit_lin, c_model_lin, linestyle='dashdot', color='#ff8000', linewidth=.7)
+
+#Axes
+plt.xlabel('Pressure [Pa]')
+plt.ylabel('$C_{DAW}$ [mm/s]')
+
+#Legend
+ax.legend(['$T_i$ adjusted','$T_i$ linear','$C_{DAW}^{exp}$'], loc='upper right', prop={'size': 8})
+#ax.legend(['Particle_tracking'], loc='upper right', prop={'size': 7})
+#ax.legend(['P15_pixel','P20','P30','P40','P50','P60','P70','P80','Fit1','Fit2'], loc='upper left', prop={'size': 7})
+
+#Edit tick 
+ax.xaxis.set_minor_locator(MultipleLocator(2.5))
+ax.yaxis.set_minor_locator(MultipleLocator(5))
+
+#adds major gridlines
+ax.grid(color='grey', linestyle='--', linewidth=0.4, alpha=0.5)
 
 #end 
 
